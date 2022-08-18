85fe78973cfdeaf669b29de7009f2493c1540319
Miguel Ruano <miguelruano35@gmail.com>
2022-08-18 10:03:43 -0600

    scrapping linux

--- flask_prod_app/flask_app.py
+++ flask_prod_app/flask_app.py
@@ -30,4 +30,5 @@ def result():
         return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000)) or 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
